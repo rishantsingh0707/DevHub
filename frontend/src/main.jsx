5e8b67cb10730e6311e4f968d760614505d66cf6
Rishant singh <rishantsingh0707@gmail.com>
2025-12-11 12:36:09 +0530

    Edited Env file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {ENV} from "../../backend/lib/env.js"
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = ENV.CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY 
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
