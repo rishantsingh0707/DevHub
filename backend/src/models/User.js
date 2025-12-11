21400881926ffffa87a5b39119906eb4c23885a3
Rishant singh <rishantsingh0707@gmail.com>
2025-12-11 14:31:36 +0530

    fixed reference error in username

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     userName: {
         type: String,
-        required: true,
     },
     email: {
         type: String,
