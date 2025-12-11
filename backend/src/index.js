56193420d4677d19fb7490457eff5245a1efe608
Rishant singh <rishantsingh0707@gmail.com>
2025-12-11 12:03:15 +0530

    added databases

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ENV } from '../lib/env.js';
+import { connectDB } from '../lib/db.js';
 const app = express();
 
 console.log('Environment Variables:', ENV);
@@ -8,6 +9,16 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
-});
+
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error(`Error starting server: ${error.message}`);
+  }
+}
+
+startServer();
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <header>
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+      <SignedIn>
+        <UserButton mode="modal" />
+      </SignedIn>
+    </header>
+  );
+
 }
 
 export default App
