9af5e9e4cca332bc0aa9b411f2eb49c9ae42afcf
Rishant singh <rishantsingh0707@gmail.com>
2025-12-12 13:39:10 +0530

    fixed invalid stream code

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -5,14 +5,14 @@ const apiKey = ENV.STREAM_API_KEY;
 const apiSecretKey = ENV.STREAM_API_SECRET;
 
 if (!apiKey || !apiSecretKey) {
-    console.error("STREAM_API_KEY and STREAM_API_SECRET are missing");
+  console.error("STREAM_API_KEY and STREAM_API_SECRET are missing");
 }
 
 export const chatClient = new StreamClient(apiKey, apiSecretKey);
 
 export const upsertStreamUser = async (userData) => {
   try {
-    await chatClient.users.upsert(userData);
+    await chatClient.upsertUsers([userData]);
     console.log("User added to Stream:", userData);
   } catch (error) {
     console.error("Stream upsert error:", error);
@@ -21,7 +21,7 @@ export const upsertStreamUser = async (userData) => {
 
 export const deleteStreamUser = async (userId) => {
   try {
-    await chatClient.users.delete(userId);
+    await chatClient.deleteUsers([userId]);
     console.log("User deleted from Stream:", userId);
   } catch (error) {
     console.error("Stream delete error:", error);
