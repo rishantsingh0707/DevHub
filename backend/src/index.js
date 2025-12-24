6a6b0456d2dc2dc8b93b8810b498404b104cd464
Rishant singh <rishantsingh0707@gmail.com>
2025-12-24 23:27:57 +0530

    changed CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,13 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
+app.use(cors({
   origin: ENV.CLIENT_URL,
   credentials: true
 }));
