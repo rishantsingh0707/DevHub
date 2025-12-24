157c2f9c79aed67619a47ac782f4b65ac1bcfa0a
Rishant singh <rishantsingh0707@gmail.com>
2025-12-24 23:08:16 +0530

    minimal cors function

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,11 +12,10 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://dev-hub-nu-ten.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: ENV.CLIENT_URL,
+  credentials: true
 }));
+
 
 app.use(clerkMiddleware())
 
