61826d6ae57578c982ba9c3a0a55749922af9617
Rishant singh <rishantsingh0707@gmail.com>
2025-12-25 18:36:05 +0530

    checking  auth

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,28 +1,27 @@
-import { clerkClient } from "@clerk/express";
+import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
+import { console } from "inspector";
 
-export const protectRoute = async (req, res, next) => {
-    try {
-        const authHeader = req.headers.authorization;
+export const protectRoute = [
+    requireAuth(),
+    async (req, res, next) => {
+        try {
+            const clerkId = req.auth.userId;
 
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            if (!clerkId) {
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+
+            const user = await User.findOne({ clerkId });
+            if (!user) {
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+
+            req.user = user;
+            next();
+        } catch (err) {
+            console.error("Auth error:", err);
             return res.status(401).json({ message: "Unauthorized" });
         }
-
-        const token = authHeader.split(" ")[1];
-
-        const payload = await clerkClient.verifyToken(token);
-        const clerkId = payload.sub;
-
-        const user = await User.findOne({ clerkId });
-        if (!user) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-
-        req.user = user;
-        next();
-    } catch (err) {
-        console.error("Auth error:", err);
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-};
+    },
+];
